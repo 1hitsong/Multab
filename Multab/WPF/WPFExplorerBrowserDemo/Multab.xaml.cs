adf59a665b70ef989728abf87bb58b58384c655d
tgpo <tgpotgpo@gmail.com>
2019-06-01 08:18:52 -0400

    Add address bar, keyboard shortcuts, up one level button

--- Multab/WPF/WPFExplorerBrowserDemo/Multab.xaml.cs
+++ Multab/WPF/WPFExplorerBrowserDemo/Multab.xaml.cs
@@ -14,6 +14,7 @@ using System.IO;
 using System.Xml;
 using Microsoft.WindowsAPICodePack.Controls.WindowsForms;
 using System.Windows.Media;
+using System.Windows.Input;
 
 namespace MulTab
 {
@@ -22,6 +23,8 @@ namespace MulTab
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Microsoft.WindowsAPICodePack.Controls.WindowsPresentationFoundation.ExplorerBrowser> eb = new List<Microsoft.WindowsAPICodePack.Controls.WindowsPresentationFoundation.ExplorerBrowser>();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -36,15 +39,41 @@ namespace MulTab
                 orderby folder.CanonicalName
                 select folder;
 
-            this.Loaded += new RoutedEventHandler(ExplorerBrowserTestWindow_Loaded);
+            this.Loaded += new RoutedEventHandler(MulTabWindow_Loaded);
         }
 
-        void ExplorerBrowserTestWindow_Loaded(object sender, RoutedEventArgs e)
+        void MulTabWindow_Loaded(object sender, RoutedEventArgs e)
         {
             TabItem item = new TabItem();
             item.Header = "Explorer";
             tabControl1.Items.Add(item);
             tabControl1.SelectedIndex = tabControl1.Items.Count - 1;
+
+            tabControl1.SelectionChanged += TabControl1_SelectionChanged;
+        }
+
+        private void TabControl1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (tabControl1.SelectedIndex == -1)
+            {
+                if (tabControl1.Items.Count == 0)
+                {
+                    TabItem item = new TabItem();
+                    item.Header = "Explorer";
+                    tabControl1.Items.Add(item);
+                    tabControl1.SelectedIndex = tabControl1.Items.Count - 1;
+                }
+                else
+                {
+                    tabControl1.SelectedIndex = 0;
+                }
+                
+            }
+
+            if (eb.Count >= tabControl1.SelectedIndex + 1)
+            {
+                textBox.Text = eb.ElementAt(tabControl1.SelectedIndex).NavigationLog.Last().ParsingName;
+            }
         }
 
         private void TabItem_Loaded(object sender, RoutedEventArgs e)
@@ -53,27 +82,87 @@ namespace MulTab
 
             if (null != tabItem.DataContext)
             {
-                var dataTemplate = (DataTemplate)tabItem.FindResource("tabItemContent");
-                var cp = new ContentPresenter();
-                cp.ContentTemplate = dataTemplate;
-                cp.Content = tabItem.DataContext;
-
-                tabItem.Content = cp;
+                eb.Add(new Microsoft.WindowsAPICodePack.Controls.WindowsPresentationFoundation.ExplorerBrowser());
+                eb.Last().ExplorerBrowserControl.NavigationLog.NavigationLogChanged += NavigationLog_NavigationLogChanged; ;
+                tabItem.Content = eb.ElementAt(tabControl1.SelectedIndex);
             }
+        }
+
+
+        private void NavigationLog_NavigationLogChanged(object sender, NavigationLogEventArgs e)
+        {
+            textBox.Text = eb.ElementAt(tabControl1.SelectedIndex).NavigationLog.Last().ParsingName;
         }
 
         private void newTab_Click(object sender, RoutedEventArgs e)
         {
+            CreateNewTab();
+        }
+
+        private void CreateNewTab()
+        {
             TabItem item = new TabItem();
-            item.ContentTemplate = TryFindResource("tabItemContent") as DataTemplate;
             item.Header = "Explorer";
             tabControl1.Items.Add(item);
             tabControl1.SelectedIndex = tabControl1.Items.Count - 1;
         }
 
-        private void closeTab_Click(object sender, RoutedEventArgs e)
+        private void CloseTab()
         {
             tabControl1.Items.RemoveAt(tabControl1.SelectedIndex);
+            eb.RemoveAt(tabControl1.SelectedIndex);
+            tabControl1.SelectedIndex = tabControl1.Items.Count - 1;
+        }
+
+        private void closeTab_Click(object sender, RoutedEventArgs e)
+        {
+            CloseTab();
+        }
+
+        private void MulTabWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.IsRepeat)
+            {
+                return;
+            }
+
+            if (e.Key == Key.T && Keyboard.IsKeyDown(Key.LeftCtrl))
+            {
+                CreateNewTab();
+                e.Handled = true;
+            }
+            else if (e.Key == Key.W && Keyboard.IsKeyDown(Key.LeftCtrl))
+            {
+                CloseTab();
+                e.Handled = true;
+            }
+        }
+
+        private void textBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                ShellObject folderToGoTo;
+
+                try
+                {
+                    folderToGoTo = ShellObject.FromParsingName(textBox.Text);
+                    eb.ElementAt(tabControl1.SelectedIndex).ExplorerBrowserControl.Navigate(folderToGoTo);
+                }
+                catch(Exception ex)
+                {
+                }
+            }
+        }
+
+        private void button_upOneFolder_Click(object sender, RoutedEventArgs e)
+        {
+            DirectoryInfo parentDirectory = Directory.GetParent(eb.ElementAt(tabControl1.SelectedIndex).NavigationLog.Last().ParsingName);
+
+            if (parentDirectory != null)
+            {
+                eb.ElementAt(tabControl1.SelectedIndex).ExplorerBrowserControl.Navigate(ShellObject.FromParsingName(Directory.GetParent(eb.ElementAt(tabControl1.SelectedIndex).NavigationLog.Last().ParsingName).ToString()));
+            }            
         }
     }
 
